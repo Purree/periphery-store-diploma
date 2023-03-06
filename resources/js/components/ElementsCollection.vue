4332b903692619ada01072ce23170fc175980d3e
Pure <kirill2283371@mail.ru>
2023-03-07 01:57:48 +0300

    Add strict width to product stub in products collection. Add random color to product stub in product collection. Add scroll styles and isScrollVisible parameter to ElementsCollection.

--- resources/js/components/ElementsCollection.vue
+++ resources/js/components/ElementsCollection.vue
@@ -5,7 +5,8 @@
                 {{ collectionTitle }}
             </div>
         </slot>
-        <div class="elements" :style="`height: ${height}`">
+        <!--        TODO: Add controls-->
+        <div class="elements" :style="`height: ${height}; ${isScrollVisible ? 'overflow-x: scroll' : ''}`">
             <slot></slot>
         </div>
     </div>
@@ -23,14 +24,54 @@ export default {
             required: false,
             type: String,
             default: '340px'
+        },
+        isScrollVisible: {
+            required: false,
+            type: Boolean,
+            default: true
         }
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.collection-container {
+    overflow: hidden;
+}
+
 .collection-title {
     font-size: 1.6rem;
     font-weight: bold;
 }
+
+.elements {
+    --scroll-background-color: var(--el-bg-color);
+    --scroll-color: var(--el-color-primary);
+    position: relative;
+    width: 100%;
+    overflow-x: hidden;
+    overflow-y: hidden;
+    white-space: nowrap;
+    scrollbar-width: thin;
+    scrollbar-color: var(--scroll-color) var(--scroll-background-color);
+
+    &::-webkit-scrollbar {
+        width: 12px;
+    }
+
+    &::-webkit-scrollbar-track {
+        background: var(--scroll-background-color);
+    }
+
+    &::-webkit-scrollbar-thumb {
+        background-color: var(--scroll-color);
+        border-radius: 20px;
+        border: 3px solid var(--scroll-background-color);
+    }
+}
+
+:deep(.elements) > * {
+    display: inline-block;
+}
+
 </style>
--- resources/js/components/home/ProductsCollection.vue
+++ resources/js/components/home/ProductsCollection.vue
@@ -1,6 +1,6 @@
 <template>
     <elements-collection :collection-title="collectionTitle">
-        <div :key="i" v-for="i in 10" style="width: 100%; height: 100%; background-color: red"></div>
+        <div :key="i" v-for="i in 10" style="width: 400px; height: 100%;" :style="'background-color: #' + Math.floor(100000 + Math.random() * 900000)"></div>
     </elements-collection>
 </template>
 
